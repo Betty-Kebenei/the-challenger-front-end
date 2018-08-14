cd0f17a5644129e08db96b43d9ff28ab0bfc1ef8
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-08-14 13:25:27 +0300

    Add charts and tables.

---
+++ scripts/readingChart.js
@@ -0,0 +1,136 @@
+var readingChart = document.getElementById("readingChart");
+readingChart.width = 500;
+readingChart.height = 300;
+  
+var ctx = readingChart.getContext("2d");
+
+function drawLine(ctx, startX, startY, endX, endY, color){
+    ctx.save();
+    ctx.strokeStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(startX,startY);
+    ctx.lineTo(endX,endY);
+    ctx.stroke();
+    ctx.restore();
+}
+
+function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height, color){
+    ctx.save();
+    ctx.fillStyle=color;
+    ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+    ctx.restore();
+}
+
+var myVinyls = {
+    "1": 10,
+    "2": 14,
+    "3": 2,
+    "4": 12,
+    "5": 10,
+    "6": 14,
+    "7": 2,
+    "8": 12,
+    "9": 10,
+    "10": 14,
+    "11": 2,
+    "12": 12,
+    "13": 10,
+    "14": 14,
+    "15": 2,
+    "16": 12,
+    "17": 10,
+    "18": 14,
+    "19": 2,
+    "20": 12,
+    "21": 10,
+    "22": 14,
+    "23": 2,
+    "24": 12,
+    "25": 10,
+    "26": 14,
+    "27": 2,
+    "28": 12,
+};
+
+var Barchart = function(options){
+    this.options = options;
+    this.canvas = options.canvas;
+    this.ctx = this.canvas.getContext("2d");
+    this.colors = options.colors;
+  
+    this.draw = function(){
+        var maxValue = 0;
+        for (var categ in this.options.data){
+            maxValue = Math.max(maxValue,this.options.data[categ]);
+        }
+        var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+        var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+ 
+        //drawing the grid lines
+        var gridValue = 0;
+        while (gridValue <= maxValue){
+            var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+            drawLine(
+                this.ctx,
+                0,
+                gridY,
+                this.canvas.width,
+                gridY,
+                this.options.gridColor
+            );
+             
+            //writing grid markers
+            this.ctx.save();
+            this.ctx.fillStyle = this.options.gridColor;
+            this.ctx.font = "bold 10px Arial";
+            this.ctx.fillText(gridValue, 10, gridY - 2);
+            this.ctx.restore();
+ 
+            gridValue+=this.options.gridScale;
+        }
+  
+        //drawing the bars
+        var barIndex = 0;
+        var numberOfBars = Object.keys(this.options.data).length;
+        var barSize = (canvasActualWidth)/numberOfBars;
+ 
+        for (categ in this.options.data){
+            var val = this.options.data[categ];
+            var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
+            drawBar(
+                this.ctx,
+                this.options.padding + barIndex * barSize,
+                this.canvas.height - barHeight - this.options.padding,
+                barSize,
+                barHeight,
+                this.colors[barIndex%this.colors.length]
+            );
+ 
+            barIndex++;
+        }
+        //drawing series name
+        this.ctx.save();
+        this.ctx.textBaseline="bottom";
+        this.ctx.textAlign="center";
+        this.ctx.fillStyle = "#000000";
+        this.ctx.font = "bold 14px Arial";
+        this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+        this.ctx.restore(); 
+  
+    }
+}
+var myBarchart = new Barchart(
+    {
+        canvas:readingChart,
+        // axisX: { title: axisX},
+        seriesName:"Dates",
+        padding:20,
+        gridScale:1,
+        gridColor:"#0000ff",
+        data:myVinyls,
+        colors:["#808080"]
+    }
+);
+myBarchart.draw();
+
+
