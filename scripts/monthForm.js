710d70b60e92071245d4902be799a372a9cb0e97
Betty-Kebenei <kebeneibetty13@gmail.com>
2018-09-04 16:42:09 +0300

    [Feature #160232650] Add GET Month form by ID.

--- scripts/monthForm.js
+++ scripts/monthForm.js
@@ -1,10 +1,24 @@
 const BaseURL = 'http://localhost:3001/api/v1/month-form';
+let month_id;
 
+//GET all month forms.
 async function getAllMonthForms() {
     try {
         const response = await axios.get(`${BaseURL}`);
         response.data.forEach((data) => {
             let node = document.createElement("LI");
+
+            //create a id attribute
+            let att1 = document.createAttribute("id"); 
+            att1.value = data._id;
+            node.setAttributeNode(att1); 
+
+            //create an onclick attribute
+            let att3 = document.createAttribute("onclick"); 
+            att3.value = "getAMonth()";
+            node.setAttributeNode(att3);
+
+            
             let textnode = document.createTextNode('From: ' + data.fromDate + ' To: ' + data.toDate );
             node.appendChild(textnode);
             document.getElementById("month-lists").appendChild(node);
@@ -15,6 +29,18 @@ async function getAllMonthForms() {
 }
 getAllMonthForms();
 
+//GET a single month form.
+async function getAMonth() {
+    let id = event.target.id;
+    try {
+        const response = await axios.get(`${BaseURL}/${id}`);
+        displayAMonthsData()
+    } catch(error) {
+        snackbar(error.response.data, "snackbar-failure");
+    }
+}
+
+//POST a month form
 async function postMonthForm() {
     const data = {
         fromDate: document.getElementById('month-form').elements[0].value,
